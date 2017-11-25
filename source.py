26ff2b9897fd0abea198738889a6f1d1d4d265b3
DeflatedPickle <DeflatedPickle@gmail.com>
2017-11-25 06:16:07 +0000

    Added modulus

--- source.py
+++ source.py
@@ -4,10 +4,12 @@ from enum import Enum
 class Actions(Enum):
     HALT = 0,
     PUSH = 1,  # Push a value onto the stack.
+    # Math:
     ADD = 2,  # Add the values in the stack.
     SUB = 3,  # Subtracts the values in the stack.
     MUL = 4,  # Multiplies the values in the stack.
     DIV = 5,  # Divides the values in the stack.
+    MOD = 6,  # Finds the remainder of a division.
 
 
 
@@ -25,6 +27,8 @@ program = [
     Actions.MUL,  # 50
     Actions.PUSH, 2,
     Actions.DIV,  # 25
+    Actions.PUSH, 3,
+    Actions.MOD,
     Actions.HALT
 ]
 
@@ -60,5 +64,10 @@ while work:
         value = stack[stack_pointer]
         stack_pointer += 1
         stack[stack_pointer] //= value
+
+    elif instruction == Actions.MOD:
+        value = stack[stack_pointer]
+        stack_pointer += 1
+        stack[stack_pointer] %= value
 
 print(stack[stack_pointer])
