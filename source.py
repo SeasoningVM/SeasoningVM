46fd677e61daefab7445b2bfdf7019dea0f8710f
DeflatedPickle <DeflatedPickle@gmail.com>
2017-11-25 17:27:10 +0000

    Improved comments

--- source.py
+++ source.py
@@ -6,20 +6,20 @@ class OPCodes(Enum):
     # Stack Modifications:
     PUSH = auto()  # Pushes a value onto the stack.
     POP = auto()  # Pops a value from the stack.
-    # Stack Movement:
     MOVE = auto()  # Moves a value to the stack.
+    # Stack Movement:
     JUMP = auto()  # Jumps to an instruction.
-    # Math:
+    # Arithmetic Operators:
     ADD = auto()  # Add the values in the stack.
     SUB = auto()  # Subtracts the values in the stack.
     MUL = auto()  # Multiplies the values in the stack.
     DIV = auto()  # Divides the values in the stack.
     MOD = auto()  # Finds the remainder of a division.
-    # Operations:
+    # Comparison Operators:
     LESS = auto()  # Determines if a value is less than another.
     MORE = auto()  # Determines if a value is more than another.
     EQUAL = auto()  # Determines if a value equals another.
-    #
+    # Logical Operators:
     AND = auto()
     OR = auto()
     NOT = auto()
@@ -31,23 +31,23 @@ stack = [0] * stack_pointer
 instruction_pointer = 0
 
 program = [
-    OPCodes.PUSH, 10,
-    OPCodes.PUSH, 20,
-    OPCodes.ADD,  # 30
-    OPCodes.PUSH, 5,
-    OPCodes.SUB,  # 25
-    OPCodes.PUSH, 2,
-    OPCodes.MUL,  # 50
-    OPCodes.PUSH, 2,
-    OPCodes.DIV,  # 25
-    OPCodes.PUSH, 3,
-    OPCodes.MOD,  # 1
-    OPCodes.PUSH, 7,
-    OPCodes.LESS,  # True/1
-    OPCodes.PUSH, 5,
-    OPCodes.MORE,  # False/0
-    OPCodes.PUSH, 0,
-    OPCodes.EQUAL,  # True/1
+    OPCodes.PUSH, 10,  # 10
+    OPCodes.PUSH, 20,  # 20, 10
+    OPCodes.ADD,       # 20, 30
+    OPCodes.PUSH, 5,   # 5,  30
+    OPCodes.SUB,       # 5,  25
+    OPCodes.PUSH, 2,   # 2,  25
+    OPCodes.MUL,       # 2,  50
+    OPCodes.PUSH, 2,   # 2,  50
+    OPCodes.DIV,       # 2,  25
+    OPCodes.PUSH, 3,   # 3,  25
+    OPCodes.MOD,       # 3,  1
+    OPCodes.PUSH, 7,   # 7,  1
+    OPCodes.LESS,      # 7,  1
+    OPCodes.PUSH, 5,   # 5,  1
+    OPCodes.MORE,      # 5,  0
+    OPCodes.PUSH, 0,   # 0,  0
+    OPCodes.EQUAL,     # 0,  1
     # OPCodes.JUMP, 1,  # Infinite loop.
     # OPCodes.JUMP, 28,  # Jumps to the end.
     OPCodes.HALT
@@ -72,7 +72,7 @@ while True:
     elif instruction == OPCodes.JUMP:
         instruction_pointer = program[instruction_pointer]
 
-    # Math:
+    # Arithmetic Operators:
 
     elif instruction == OPCodes.ADD:
         value = stack[stack_pointer]
@@ -99,7 +99,7 @@ while True:
         stack_pointer += 1
         stack[stack_pointer] %= value
 
-    # Operations:
+    # Comparison Operations:
 
     elif instruction == OPCodes.LESS:
         value = stack[stack_pointer]
@@ -116,5 +116,5 @@ while True:
         stack_pointer += 1
         stack[stack_pointer] = int(stack[stack_pointer] == value)
 
-print(stack)
-print(stack[stack_pointer])
+    print(stack)
+    print(stack[stack_pointer])
