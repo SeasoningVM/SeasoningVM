2a73dd1ee9db1cbbe37329354d879a61ab06c1ad
DeflatedPickle <DeflatedPickle@gmail.com>
2017-11-25 21:04:43 +0000

    Added a few examples

--- source.py
+++ source.py
@@ -1,12 +1,15 @@
 from enum import Enum, auto
 
 
-class OPCodes(Enum):
+# OPCODE
+class OPRAND(Enum):
     HALT = auto()  # Stops the program.
     # Stack Modifications:
     PUSH = auto()  # Pushes a value onto the stack.
-    POP = auto()  # Pops a value from the stack.
-    MOVE = auto()  # Moves a value to the stack.
+    # TODO: POP = auto()  # Pops a value from the stack.
+    # TODO: MOVE = auto()  # Moves a value to the stack.
+    LOAD = auto()  # Loads a value from the stack.
+    STORE = ()  # Stores a value in the stack.
     # Stack Movement:
     JUMP = auto()  # Jumps to an instruction.
     # Arithmetic Operators:
@@ -20,9 +23,9 @@ class OPCodes(Enum):
     MORE = auto()  # Determines if a value is more than another.
     EQUAL = auto()  # Determines if a value equals another.
     # Logical Operators:
-    AND = auto()
-    OR = auto()
-    NOT = auto()
+    # TODO: AND = auto()
+    # TODO: OR = auto()
+    # TODO: NOT = auto()
 
 
 stack_pointer = 32
@@ -30,34 +33,48 @@ stack = [None] * stack_pointer
 instruction_pointer = 0
 
 program = [
-    OPCodes.PUSH, 10,  # 10
-    OPCodes.PUSH, 20,  # 20, 10
-    OPCodes.ADD,       # 20, 30
-    OPCodes.PUSH, 5,   # 5,  30
-    OPCodes.SUB,       # 5,  25
-    OPCodes.PUSH, 2,   # 2,  25
-    OPCodes.MUL,       # 2,  50
-    OPCodes.PUSH, 2,   # 2,  50
-    OPCodes.DIV,       # 2,  25
-    OPCodes.PUSH, 3,   # 3,  25
-    OPCodes.MOD,       # 3,  1
-    OPCodes.PUSH, 7,   # 7,  1
-    OPCodes.LESS,      # 7,  1
-    OPCodes.PUSH, 5,   # 5,  1
-    OPCodes.MORE,      # 5,  0
-    OPCodes.PUSH, 0,   # 0,  0
-    OPCodes.EQUAL,     # 0,  1
-    # OPCodes.JUMP, 1,  # Infinite loop.
-    # OPCodes.JUMP, 28,  # Jumps to the end.
-    OPCodes.HALT
+    OPRAND.PUSH, 10,  # 10
+    OPRAND.PUSH, 20,  # 20, 10
+    OPRAND.ADD,       # 20, 30
+    OPRAND.PUSH, 5,   # 5,  30
+    OPRAND.SUB,       # 5,  25
+    OPRAND.PUSH, 2,   # 2,  25
+    OPRAND.MUL,       # 2,  50
+    OPRAND.PUSH, 2,   # 2,  50
+    OPRAND.DIV,       # 2,  25
+    OPRAND.PUSH, 3,   # 3,  25
+    OPRAND.MOD,       # 3,  1
+    OPRAND.PUSH, 7,   # 7,  1
+    OPRAND.LESS,      # 7,  1
+    OPRAND.PUSH, 5,   # 5,  1
+    OPRAND.MORE,      # 5,  0
+    OPRAND.PUSH, 0,   # 0,  0
+    OPRAND.EQUAL,     # 0,  1
+    # OPRAND.JUMP, 1,  # Infinite loop.
+    # OPRAND.JUMP, 28,  # Jumps to the end.
+    OPRAND.HALT
 ]
 
 # 5 + 10
 example_add = [
-    OPCodes.PUSH, 5,
-    OPCodes.PUSH, 10,
-    OPCodes.ADD,
-    OPCodes.HALT
+    OPRAND.PUSH, 5,
+    OPRAND.PUSH, 10,
+    OPRAND.ADD,
+    OPRAND.HALT
+]
+
+example_loop = [
+    OPRAND.PUSH, 1,
+    OPRAND.PUSH, 1,
+    OPRAND.ADD,
+    OPRAND.JUMP, 2,
+    OPRAND.HALT
+]
+
+example_store_load = [
+    OPRAND.PUSH, 5,
+    OPRAND.PUSH, 5,
+    OPRAND.HALT
 ]
 
 while True:
@@ -66,61 +83,71 @@ while True:
     instruction = working[instruction_pointer]  # The current instruction is at the instruction pointer.
     instruction_pointer += 1  # Increase the instruction pointer (move down the list).
 
-    if instruction == OPCodes.HALT:
+    if instruction == OPRAND.HALT:
         break
 
     # Stack Modifications:
 
-    elif instruction == OPCodes.PUSH:
+    elif instruction == OPRAND.PUSH:
         stack_pointer -= 1
         stack[stack_pointer] = working[instruction_pointer]
         instruction_pointer += 1
 
+    elif instruction == OPRAND.LOAD:
+        stack_index = stack[stack_pointer]
+        stack_pointer += 1
+        stack[stack_index] = stack[stack_pointer]
+        stack_pointer += 1
+
+    elif instruction == OPRAND.STORE:
+        stack_index = stack[stack_pointer]
+        stack[stack_pointer] = stack[stack_index]
+
     # Stack Movement:
 
-    elif instruction == OPCodes.JUMP:
+    elif instruction == OPRAND.JUMP:
         instruction_pointer = working[instruction_pointer]
 
     # Arithmetic Operators:
 
-    elif instruction == OPCodes.ADD:
+    elif instruction == OPRAND.ADD:
         value = stack[stack_pointer]
         stack_pointer += 1
         stack[stack_pointer] += value
 
-    elif instruction == OPCodes.SUB:
+    elif instruction == OPRAND.SUB:
         value = stack[stack_pointer]
         stack_pointer += 1
         stack[stack_pointer] -= value
 
-    elif instruction == OPCodes.MUL:
+    elif instruction == OPRAND.MUL:
         value = stack[stack_pointer]
         stack_pointer += 1
         stack[stack_pointer] *= value
 
-    elif instruction == OPCodes.DIV:
+    elif instruction == OPRAND.DIV:
         value = stack[stack_pointer]
         stack_pointer += 1
         stack[stack_pointer] //= value
 
-    elif instruction == OPCodes.MOD:
+    elif instruction == OPRAND.MOD:
         value = stack[stack_pointer]
         stack_pointer += 1
         stack[stack_pointer] %= value
 
     # Comparison Operations:
 
-    elif instruction == OPCodes.LESS:
+    elif instruction == OPRAND.LESS:
         value = stack[stack_pointer]
         stack_pointer += 1
         stack[stack_pointer] = int(stack[stack_pointer] < value)
 
-    elif instruction == OPCodes.MORE:
+    elif instruction == OPRAND.MORE:
         value = stack[stack_pointer]
         stack_pointer += 1
         stack[stack_pointer] = int(stack[stack_pointer] > value)
 
-    elif instruction == OPCodes.EQUAL:
+    elif instruction == OPRAND.EQUAL:
         value = stack[stack_pointer]
         stack_pointer += 1
         stack[stack_pointer] = int(stack[stack_pointer] == value)
