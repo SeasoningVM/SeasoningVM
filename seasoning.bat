25459f0be5e92ac8e49e5961b9d485c020cb3328
DeflatedPickle <DeflatedPickle@gmail.com>
2017-11-27 01:41:39 +0000

    Added a command to exit

--- seasoning.bat
+++ seasoning.bat
@@ -1,7 +1,23 @@
 @echo off
+setlocal EnableDelayedExpansion
+
 echo Seasoning Virtual Machine 1.0.0
-echo Type "credits", "license" or "info" for more information.
+echo Type "credits", "license" or "info" for more information. Type "exit" to leave.
+
 :ask
-set /p File= ">>> "
+set /p File=">>> "
+echo %File%
+if [%File%] == [exit] goto exit
+
 python -u source.py %File%
+
 goto ask
+
+:credits
+
+:license
+
+:info
+
+:exit
+echo Returning to the world.
