3ef29485f5eda9f740cbdb7945a7c28d1957fb55
Tristan <avaitorboy@gmail.com>
2017-11-29 14:49:34 -0500

    Added file reader and interpreter modes

--- seasoning.bat
+++ seasoning.bat
@@ -2,31 +2,42 @@
 
 echo Seasoning Virtual Machine [Version 1.0.0]
 echo (c) 2017, Seaoning-Virtual-Machine. BSD 3-Clause.
-echo/
+echo.
 echo Type "credits", "license" or "info" for more information. Type "exit" to leave.
+echo Type "file" to enter file reader mode or enter "insert" to enter an ASM interpreter mode
 
-set Source="
+set Source=""
 
 :ask
-set /p Insert=">>> "
+set /p Mode=">>> "
 
-if [%Insert%] == [exit] goto exit
-if [%Insert%] == [credits] goto credits
-if [%Insert%] == [license] goto license
-if [%Insert%] == [info] goto info
-if [%Insert%] == [exit] goto exit
+if [%Mode%] == [exit] goto exit
+if [%Mode%] == [credits] goto credits
+if [%Mode%] == [license] goto license
+if [%Mode%] == [info] goto info
+if [%Mode%] == [end] goto end
+if [%Mode%] == [insert] echo ASM Interpreter Mode & goto interpreter
+if [%Mode%] == [file] echo ASM File Reader & goto fileReader
+goto ask
 
-if [%Insert%] == [end] goto end
+:interpreter
+set /p Input=">>> "
+if "%Input%" == "EOF" goto exit
+if "%Input%" == "" goto interpreter
+python source.py %Input% "source"
+set Input=""
+goto interpreter
 
-if exist %Insert% (
-    python source.py %Insert% "file"
+:filereader
+set /p File=">>> "
+if "%File%" == "" echo No file entered! & goto filereader
+if exist %File% (
+    python source.py %File% "file"
 ) else (
     rem echo This file does not exist.
-
-    set Source=%Source%%Insert%,
+    set Source=%Source%%File%,
 )
-
-goto ask
+goto filereader
 
 :end
 set Source=%Source%"
