3d4869db7998c4704f61f4d2a87b0a8274be8f31
Dibbo <DeflatedPickle@gmail.com>
2017-11-29 15:54:30 +0000

    Added a basic assembler

--- assembler.py
+++ assembler.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+def assemble(file, memory_size):
+    file.write(0xC0DE.to_bytes(2, byteorder='little'))
+    file.write(memory_size.to_bytes(4, byteorder='little'))
+
+
+if __name__ == "__main__":
+    with open("example.sbc", "wb+") as bytecode:
+        assemble(bytecode, 64)
--- opcode.py
+++ opcode.py
@@ -4,6 +4,7 @@
 from enum import Enum
 from enum import auto
 
+
 class OPRAND(Enum):
     HALT = auto()  # Stops the program.
     # Stack Modifications:
